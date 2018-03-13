dc51b363f5412c1759e487385d0a4bc18dfc4073
zyuiop <zyuiop@zyuiop.net>
2018-03-13 21:21:11 +0100

    Exo3 more or less done

--- week3/03-recouvrement.c
+++ week3/03-recouvrement.c
@@ -13,29 +13,59 @@ int remplitGrille(Grille grille, size_t ligne, size_t colonne, char direction, s
 
 void init_grid(Grille grille);
 
+void print_grid(Grille grille);
+
 int main(void) {
     Grille grille;
+    init_grid(grille);
 
     int x = 0;
     int y = 0;
 
     do {
+        do {
+            printf("Entrez coordonnées x [1-10] : ");
+            scanf("%d", &x);
+        } while (x > DIM);
 
+        do {
+            printf("Entrez coordonnées y [1-10] : ");
+            scanf("%d", &y);
+        } while (y > DIM);
+
+        char chr;
+        do {
+            printf("Entrez direction (N,S,E,O) : ");
+            scanf("%c", &chr);
+        } while (chr != 'E' && chr != 'W' && chr != 'S' && chr != 'N');
+
+        int taille;
+        do {
+            printf("Entrez taille : ");
+            scanf("%d", &taille);
+        } while (taille > DIM);
+
+        if (remplitGrille(grille, y - 1, x - 1, chr, taille) == 1) {
+            printf("Ok !\n");
+        } else {
+            printf("ça rentre pas...\n");
+        }
+
+        print_grid(grille);
     } while (x >= 0 && y >= 0);
 
-    /*
-     * définissez une variable nommée grille, correspondant à un tableau de char, de taille fixe, à deux dimensions, avec DIM comme paramètre de taille pour chaque dimension (c'est-à-dire une grille carrée).
-Le caractère '#' dans une case [i][j] de cette grille représente le fait qu'un (bout d')objet occupe la case de coordonnées (i, j) et le caractère '.' que la case est vide (utilisez des constantes).
-Utilisez la fonction précédente pour remplir interactivement la grille, en demandant à l'utilisateur de spécifier la position, la taille et la direction des objets à placer.
-Indiquez à chaque fois à l'utilisateur si l'élément a pu ou non être placé dans la grille.
-Le remplissage se termine lorsque l'utilisateur entre une coordonnée strictement inférieure à 0.
-Terminer alors en "dessinant" la grille : afficher un '.' si la case est vide et un '#' si la case est occupée.
-     */
+    printf("Fini\n\n");
+
+    print_grid(grille);
 }
 
 int remplitGrille(Grille grille, size_t ligne, size_t colonne, char direction, size_t longueur) {
     switch (direction) {
-        case 'e':
+        case 'E':
+            if (colonne + longueur > DIM) {
+                return 0;
+            }
+
             for (int i = 0; i < longueur; ++i) {
                 if (grille[ligne][colonne + i] != EMPTY) {
                     return 0;
@@ -46,7 +76,11 @@ int remplitGrille(Grille grille, size_t ligne, size_t colonne, char direction, s
                 grille[ligne][colonne + i] = FILLED;
             }
             break;
-        case 's':
+        case 'S':
+            if (ligne + longueur > DIM) {
+                return 0;
+            }
+
             for (int i = 0; i < longueur; ++i) {
                 if (grille[ligne + i][colonne] != EMPTY) {
                     return 0;
@@ -57,13 +91,15 @@ int remplitGrille(Grille grille, size_t ligne, size_t colonne, char direction, s
                 grille[ligne + i][colonne] = FILLED;
             }
             break;
-        case 'w':
+        case 'W':
             return remplitGrille(grille, ligne, colonne - longueur, 'e', longueur);
-        case 'n':
+        case 'N':
             return remplitGrille(grille, ligne - longueur, colonne, 's', longueur);
         default:
             return 0;
     }
+
+    return 1;
 }
 
 void init_grid(Grille grille) {
@@ -71,5 +107,15 @@ void init_grid(Grille grille) {
         for (int j = 0; j < DIM; ++ j) {
             grille[i][j] = EMPTY;
         }
+    }
+}
+
+void print_grid(Grille grille) {
+    for (int i = 0; i < DIM; ++ i) {
+        for (int j = 0; j < DIM; ++ j) {
+            printf("%c", grille[i][j]);
+        }
+
+        printf("\n");
     }
 }
