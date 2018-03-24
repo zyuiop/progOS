e2707a341813b0c537dcf0f67301d104a363d077
zyuiop <zyuiop@zyuiop.net>
2018-03-24 01:31:37 +0100

    Week 5 ex 2

--- week5/02-selecteur.c
+++ week5/02-selecteur.c
@@ -2,3 +2,23 @@
 // Created by zyuiop on 24/03/18.
 //
 
+#include <stdio.h>
+
+int main(void) {
+    double v1 = 1, v2 = 2, v3 = 3;
+    double* choice;
+
+    int p;
+    do {
+        printf("Choisissez un nombre entre 1 et 3: ");
+        scanf("%d", &p);
+    } while (p < 1 || p > 3);
+
+    switch (p) {
+        case 1: choice = &v1; break;
+        case 2: choice = &v2; break;
+        case 3: choice = &v3;
+    }
+
+    printf("Variable correspondante: %lf, addr %ld", *choice, choice);
+}
